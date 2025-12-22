9a3f7fe531d6e4ab5859d048e54d93a596569a39
SmileChaoLee <leechao1959@gmail.com>
2025-12-22 10:27:51 -0500

    Converted BouncyBallApp.java to Kotlin

--- app/src/main/java/com/smile/bouncyball/BouncyBallApp.kt
+++ app/src/main/java/com/smile/bouncyball/BouncyBallApp.kt
@@ -1,53 +1,56 @@
-package com.smile.bouncyball;
+package com.smile.bouncyball
 
-import android.app.Application;
-import android.content.Context;
-import android.os.Handler;
-import android.os.Looper;
+import android.app.Application
+import com.facebook.ads.AudienceNetworkAds
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.initialization.InitializationStatus
+import com.smile.bouncyball.tools.LogUtil
 
-import com.facebook.ads.AudienceNetworkAds;
-import com.google.android.gms.ads.MobileAds;
-import com.smile.bouncyball.tools.LogUtil;
-import com.smile.smilelibraries.facebook_ads_util.FacebookInterstitial;
-import com.smile.smilelibraries.google_ads_util.AdMobInterstitial;
+class BouncyBallApp : Application() {
 
-public class BouncyBallApp extends Application {
-    public static String DATABASE_NAME = "bouncy_ball.db";
-    public static String googleAdMobBannerID = "";
-    public FacebookInterstitial facebookAds;
-    public AdMobInterstitial googleInterstitialAd;
+    companion object {
+        private const val TAG = "BouncyBallApp"
+        var DATABASE_NAME: String = "bouncy_ball.db"
+        var googleAdMobBannerID: String = ""
+    }
 
-    private static final String TAG = "BouncyBallApp";
+    /*  No Interstitial ad for now
+    var facebookAds: FacebookInterstitial? = null
+    var googleInterstitialAd: AdMobInterstitial? = null
+    */
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        Context ctx = getApplicationContext();
+    override fun onCreate() {
+        super.onCreate()
+        val ctx = applicationContext
+
         // Facebook ads (Interstitial ads)
-
-        AudienceNetworkAds.initialize(this);
-        String facebookPlacementID = "253834931867002_253835175200311";
-        facebookAds = new FacebookInterstitial(ctx, facebookPlacementID);
+        AudienceNetworkAds.initialize(this)
+        // val facebookPlacementID = "253834931867002_253835175200311"
+        // facebookAds = FacebookInterstitial(ctx, facebookPlacementID)
 
         // Google AdMob
         // String googleAdMobAppID = getString(R.string.google_AdMobAppID);
-        String googleAdMobInterstitialID = "ca-app-pub-8354869049759576/8555812568";
-        MobileAds.initialize(ctx, initializationStatus ->
-                LogUtil.d(TAG, "Google AdMob was initialized successfully."));
-        googleInterstitialAd = new AdMobInterstitial(ctx, googleAdMobInterstitialID);
-        googleInterstitialAd.loadAd(); // load first ad
-        googleAdMobBannerID = "ca-app-pub-8354869049759576/7770302361";
-
-        final Handler adHandler = new Handler(Looper.getMainLooper());
-        final Runnable adRunnable = () -> {
-            adHandler.removeCallbacksAndMessages(null);
+        val googleAdMobInterstitialID = "ca-app-pub-8354869049759576/8555812568"
+        MobileAds.initialize(
+            ctx) { status: InitializationStatus? ->
+            LogUtil.d(TAG, "Google AdMob was initialized successfully.")
+        }
+        /*
+        googleInterstitialAd = AdMobInterstitial(ctx, googleAdMobInterstitialID)
+        googleInterstitialAd!!.loadAd() // load first ad
+        val adHandler = Handler(Looper.getMainLooper())
+        val adRunnable = Runnable {
+            adHandler.removeCallbacksAndMessages(null)
             if (googleInterstitialAd != null) {
-                googleInterstitialAd.loadAd(); // load first google ad
+                googleInterstitialAd!!.loadAd() // load first google ad
             }
             if (facebookAds != null) {
-                facebookAds.loadAd();   // load first facebook ad
+                facebookAds!!.loadAd() // load first facebook ad
             }
-        };
-        adHandler.postDelayed(adRunnable, 1000);
+        }
+        adHandler.postDelayed(adRunnable, 1000)
+        */
+
+        googleAdMobBannerID = "ca-app-pub-8354869049759576/7770302361"
     }
 }
